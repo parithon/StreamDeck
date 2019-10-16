110143f49830f7ddab2387d5cf8e8e49074f2fdb
Anthony Conrad <8602418+parithon@users.noreply.github.com>
2019-10-16 16:08:55 -0700

    Added C# projects

---
+++ StreamDeck.Template/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StreamDeck.Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
